9a2c752828a35b82fdcc89822a282d75498ad4dc
sgujuluva <sanghee04@gmail.com>
2022-10-19 15:54:55 +0200

    front end styling done

--- client/src/components/List/ListItem.js
+++ client/src/components/List/ListItem.js
@@ -2,14 +2,19 @@ import styles from './ListItem.module.css';
 
 export default function ListItem(props) {
     return (
+      
+
         <div className={styles.container}>
         
-            <ul className="companies-list">
+            <ul>
                 <li>{props.name}</li>
                 <li>{props.email_address}</li>
                 
             </ul>
 
         </div>
+        
     )
 }
+
+{/*  <div className="list-container"> </div> */}
