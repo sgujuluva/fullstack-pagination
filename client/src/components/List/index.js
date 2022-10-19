3ae09d708cc3c377c6a1702604a71c79275b4523
sgujuluva <sanghee04@gmail.com>
2022-10-19 15:42:11 +0200

    previous and next button working

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -4,15 +4,15 @@ import axios from "axios";
 import styles from "./index.module.css";
 
 export default function List() {
-  const limit = 25;
+  const limit = 10;
   const [list, setList] = useState([]);
   const [skip, setSkip] = useState(0);
 
   //getting data from backend using axios
   const getCompaniesListFromServer = async () => {
     await axios
-      .get(`http://localhost:3001/companies/list?skip=${skip}&limit=${limit}`)
-      .then((response) => setList(response.data));
+      .get(`http://localhost:3001/companies/list?limit=${limit}&skip=${skip}`)
+      .then((response) => setList(response.data.getAllCompanies));
   };
 
   //function for previous button
@@ -24,9 +24,15 @@ export default function List() {
       setSkip(newSkip);
     }
   };
+
+  //function for next button
+  const handleNext = () => {
+    setSkip(skip + limit);
+  };
+
   useEffect(() => {
     getCompaniesListFromServer();
-  }, []);
+  }, [skip]);
 
   return (
     <div className={styles.container}>
@@ -40,14 +46,14 @@ export default function List() {
           <h1>
             This is the list! Displaying listings {skip} to {skip + limit}
           </h1>
-          <div className={styles.buttons}>
-            <button onClick={handlePrevious}>Previous</button>
-            <button onClick={handleNext}>Next</button>
-          </div>
 
           {list.map((item) => (
             <ListItem {...item} />
           ))}
+          <div className={styles.buttons}>
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
         </>
       )}
     </div>
