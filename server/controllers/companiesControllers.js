3d240ca90bd6be3e5306114050fb1aed5bd34dc4
sgujuluva <sanghee04@gmail.com>
2022-10-19 14:09:52 +0200

    sorted companies by name using sort

--- server/controllers/companiesControllers.js
+++ server/controllers/companiesControllers.js
@@ -10,8 +10,14 @@ export const getCompaniesList = async (req, res) => {
     return res
       .status(200)
       .json({ message: "List of all companies", getAllCompanies });
-
   } catch (error) {
-    return res.status(500).json({message:error.message});
+    return res.status(500).json({ message: error.message });
   }
+};
+
+//sorting the companies name in ascending order
+export const sortingCompanyName = async (req, res) => {
+    const limit = Number(req.query.limit) || 50
+  const sortName = await Companies.find().select("name").limit(limit).sort({ name: 1 }).lean();
+  return res.status(200).json(sortName);
 };
--- server/routes/companies.js
+++ server/routes/companies.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {getCompaniesList }from "../controllers/companiesControllers.js"
+import {getCompaniesList, sortingCompanyName }from "../controllers/companiesControllers.js"
 
 const router = express.Router();
 
-//creating route
+//getting list of companies
 router.get("/list",getCompaniesList)
+
+//sorting the companies name
+router.get("/sort",sortingCompanyName )
 
 export default router
