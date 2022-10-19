cc52ec4fa46fe000325369561bc68608e5d6cd59
sgujuluva <sanghee04@gmail.com>
2022-10-19 11:52:22 +0200

    schema setup

--- server/models/companiesSchema.js
+++ server/models/companiesSchema.js
@@ -2,4 +2,16 @@ import mongoose from "mongoose";
 
 const {Schema, model} = mongoose;
 
-const 
+const companiesSchema = new Schema({
+    name:{type:String, required:true,unique:true},
+    homepage_url:{type:String, required:true},
+    category_code: String,
+    founded_year:Number,
+    email_address :{type:String, required:true},
+    description:String,
+    products:[
+    {name:String,
+        permalink:String
+    }
+]
+})
